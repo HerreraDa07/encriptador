84ca0b01fc127bc3b311266614c8bb45260fc102
HerreraDa07 <dsherrera650@soy.sena.edu.co>
2024-07-19 14:22:08 -0500

    encriptador-version-1.0

---
+++ script/script.js
@@ -0,0 +1,17 @@
+/* function borrarDatosCaja() {
+let textoDigitado = document.getElementById('entradaUsuario').value;
+let textoParaEncriptar = textoDigitado;
+elementosHtml('h3', textoParaEncriptar)
+}
+function copiarTexto() {
+let textoCopiado = document.getElementById('encriptador').innerText;
+console.log(textoCopiado);
+}
+function elementosHtml(elemento, texto) {
+let elementoPagina = document.querySelector(elemento);
+elementoPagina.innerHTML = texto;
+}
+function programaIniciado() {
+elementosHtml('h3', 'Aun no ha encriptado ningun mensaje');
+}
+programaIniciado(); */
