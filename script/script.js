9a275332501e0f521c6b39eeddf3501882f200aa
David Herrera <dsherrera650@soy.sena.edu.co~>
2024-07-21 22:26:50 -0500

    encriptador-version-1.6

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
-/* function borrarDatosCaja() {
-let textoDigitado = document.getElementById('entradaUsuario').value;
-let textoParaEncriptar = textoDigitado;
-elementosHtml('h3', textoParaEncriptar)
+function borrarDatosCaja() {
+    let textoDigitado = document.getElementById('entradaUsuario').value;
+    let textoParaEncriptar = textoDigitado;
+    elementosHtml('textoEncriptado', textoParaEncriptar)
 }
 function copiarTexto() {
-let textoCopiado = document.getElementById('encriptador').innerText;
-console.log(textoCopiado);
+    let textoCopiado = document.getElementById('encriptador').innerText;
+    console.log(textoCopiado);
 }
-function elementosHtml(elemento, texto) {
-let elementoPagina = document.querySelector(elemento);
-elementoPagina.innerHTML = texto;
+function elementosHtml(id, texto) {
+    let elementoPagina = document.getElementById(id);
+    elementoPagina.innerHTML = texto;
 }
 function programaIniciado() {
-elementosHtml('h3', 'Aun no ha encriptado ningun mensaje');
+    elementosHtml('textoEncriptado', 'Aun no ha encriptado ningun mensaje');
 }
-programaIniciado(); */
+programaIniciado();
