07d1360f3b485b6bfd7460dbad29a9fed16a6e0d
David Herrera <dsherrera650@soy.sena.edu.co~>
2024-07-22 23:38:38 -0500

    encriptador-version-1.9

--- script/script.js
+++ script/script.js
@@ -1,23 +1,10 @@
 let texto;
-function encriptarMensaje() {
-    let textoDigitado = document.getElementById('entradaUsuario').value;
-    texto = textoDigitado;
-    if (texto != '') {
-        elementosTextoHtml('textoRecibido', texto);
-    }
-    else {
-        elementosTextoHtml('textoRecibido', 'No ha digitado');
-    }
-}
 function copiarTexto() {
     let textoRecibido = document.getElementById('textoRecibido').innerText;
-}
-function elementosTextoHtml(id, texto) {
-    let elementoPagina = document.getElementById(id);
-    elementoPagina.innerHTML = texto;
-}
-function programaIniciado() {
-    elementosTextoHtml('textoRecibido', 'Aun no ha digitado ningun mensaje. Por favor, ingresa un texto.');
+    navigator.clipboard.writeText(textoRecibido).then(() => {
+        elementosTextoHtml('textoRecibido', '');
+        document.getElementById('botonCopiar').setAttribute('disabled', true);
+    })
 }
 function elementoIconoHtml(id) {
     let botonPresionado = id;
@@ -28,18 +15,43 @@ function elementoIconoHtml(id) {
         }
         else {
             document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-candado-cerrado.svg)');
+            document.getElementById('botonCopiar').removeAttribute('disabled');
         }
     }
     else if (botonPresionado == 'botonDesencriptar') {
+        encriptarMensaje();
         if (texto == '') {
             document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-simbolo-pregunta.svg)');
         }
         else {
             document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-candado-abierto.svg)');
+            document.getElementById('botonCopiar').removeAttribute('disabled');
         }
+    }
+    else if (botonPresionado == 'botonCopiar') {
+        copiarTexto();
+    }
+}
+function elementosTextoHtml(id, texto) {
+    let elementoPagina = document.getElementById(id);
+    elementoPagina.innerHTML = texto;
+}
+function encriptarMensaje() {
+    let textoDigitado = document.getElementById('entradaUsuario').value;
+    texto = textoDigitado;
+    if (texto != '') {
+        elementosTextoHtml('textoRecibido', texto);
+        document.getElementById('entradaUsuario').value = '';
+    }
+    else {
+        elementosTextoHtml('textoRecibido', 'No ha digitado');
     }
 }
 function presionoBoton(id) {
     elementoIconoHtml(id);
+}
+function programaIniciado() {
+    elementosTextoHtml('textoRecibido', 'Aun no ha digitado ningun mensaje. Por favor, ingresa un texto.');
+    document.getElementById('botonCopiar').setAttribute('disabled', true);
 }
 programaIniciado();
