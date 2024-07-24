48626a7c681a721b1867771a79985756ede06b56
David Herrera <dsherrera650@soy.sena.edu.co~>
2024-07-24 00:28:33 -0500

    encriptador-version-2.0

--- script/script.js
+++ script/script.js
@@ -1,57 +1,60 @@
-let texto;
+function cambiarTextosHtml(id, texto) {
+    let textoHtml = document.getElementById(id); /* Guarda el id (recibido) de un objeto html en una variable modificable */
+    textoHtml.innerHTML = texto; /* accede al valor tipo string del objeto id (recibido) y le da el valor de texto (recibido) */
+}
 function copiarTexto() {
-    let textoRecibido = document.getElementById('textoRecibido').innerText;
-    navigator.clipboard.writeText(textoRecibido).then(() => {
-        elementosTextoHtml('textoRecibido', '');
-        document.getElementById('botonCopiar').setAttribute('disabled', true);
-    })
+    let textoCopiado = document.getElementById('textoRecibido').innerText; /* Guarda el valor del mensaje (string) del bloque receptor */
+    navigator.clipboard.writeText(textoCopiado).then(() => /* Copia en la papelera el valor de la variable modificable*/ {
+        cambiarTextosHtml('textoRecibido', ''); /* Bloque receptor: Borra el mensaje */
+        document.getElementById('botonCopiar').setAttribute('disabled', true); /* Desactiva el boton de copiar */
+    });
 }
-function elementoIconoHtml(id) {
-    let botonPresionado = id;
-    if (botonPresionado == 'botonEncriptar') {
-        encriptarMensaje();
-        if (texto == '') {
-            document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-simbolo-pregunta.svg)');
-        }
-        else {
-            document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-candado-cerrado.svg)');
-            document.getElementById('botonCopiar').removeAttribute('disabled');
-        }
-    }
-    else if (botonPresionado == 'botonDesencriptar') {
-        encriptarMensaje();
-        if (texto == '') {
-            document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-simbolo-pregunta.svg)');
-        }
-        else {
-            document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-candado-abierto.svg)');
-            document.getElementById('botonCopiar').removeAttribute('disabled');
-        }
-    }
-    else if (botonPresionado == 'botonCopiar') {
-        copiarTexto();
-    }
+function encriptar(texto) {
+    let remplazarVocales = { 'a': 'ai', 'e': 'enter', 'i': 'imes', 'o': 'ober', 'u': 'ufat' };
+    /* Variable que contiene las vocales y las palabras claves para encriptar */
+    return texto.replace(/[aeiou]/g, function (remplazar) {
+        /* Validacion de vocales en el mensaje (recibido) y remplazo por palabras claves */
+        return remplazarVocales[remplazar];
+        /* Accede a la variable y obtiene las palabras claves*/
+    });
 }
-function elementosTextoHtml(id, texto) {
-    let elementoPagina = document.getElementById(id);
-    elementoPagina.innerHTML = texto;
-}
-function encriptarMensaje() {
-    let textoDigitado = document.getElementById('entradaUsuario').value;
-    texto = textoDigitado;
-    if (texto != '') {
-        elementosTextoHtml('textoRecibido', texto);
-        document.getElementById('entradaUsuario').value = '';
-    }
-    else {
-        elementosTextoHtml('textoRecibido', 'No ha digitado');
-    }
+function paginaIniciada() {
+    document.getElementById('botonCopiar').setAttribute('disabled', true); /* Desactiva el boton de Copiar */
+    document.getElementById('entradaUsuario').addEventListener("input", function () {
+        this.value = this.value.replace(/[^a-zA-Z ]/g, ''); /* Impide que se usen caracteres especiales */
+        this.value = this.value.toLowerCase(); /* Convierte las letras mayusculas en letras minusculas */
+    });
+    cambiarTextosHtml('textoRecibido', 'Ningún mensaje ha sido digitado. Ingresa el texto que desees encriptar o desencriptar.');
+    /* Bloque receptor: Mensaje inicial */
 }
 function presionoBoton(id) {
-    elementoIconoHtml(id);
+    let boton = id; /* Le da el valor del id (recibido) a una nueva variable modificable */
+    if (boton == 'botonEncriptar') {
+        document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-candado-cerrado.svg)');
+        /* Bloque receptor: Coloca icono del candado cerrado */
+        recibirTexto(); /* Al presionar el boton de encriptar manda a llamar a la funcion que guarda el mensaje del textarea */
+    }
+    else if (boton == 'botonDesencriptar') {
+        document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-candado-abierto.svg)');
+        /* Bloque receptor: Coloca icono del candado abierto */
+        recibirTexto(); /* Al presionar el boton de desencriptar manda a llamar a la funcion que guarda el mensaje del textarea */
+    }
+    else if (boton = 'botonCopiar') {
+        copiarTexto(); /* Al presionar el boton de copiar manda a llamar a la funcion para copiar texto */
+    }
 }
-function programaIniciado() {
-    elementosTextoHtml('textoRecibido', 'Aun no ha digitado ningun mensaje. Por favor, ingresa un texto.');
-    document.getElementById('botonCopiar').setAttribute('disabled', true);
+function recibirTexto() {
+    let textoDigitado = document.getElementById('entradaUsuario').value; /* Extrae el mensaje del textarea */
+    let mensaje = encriptar(textoDigitado); /* Pasa el valor tipo string del textarea a una nueva variable modificable */
+    if (mensaje != '') {
+        document.getElementById('botonCopiar').removeAttribute('disabled'); /* Activa el boton de Copiar */
+        document.getElementById('entradaUsuario').value = ''; /* Borra el valor que tenga el textarea */
+        cambiarTextosHtml('textoRecibido', mensaje); /* Envia el mensaje a  */
+    } else {
+        document.documentElement.style.setProperty('--icono-tres', 'url(/recursos/icono-simbolo-pregunta.svg)');
+        /* Bloque receptor: Coloca icono del simbolo de pregunta */
+        cambiarTextosHtml('textoRecibido', '¡Error! No ha digitado ningún mensaje. Por favor, ingrese el texto que desea encriptar o desencriptar.');
+        /* Bloque receptor: Mensaje al no recibir ningun texto */
+    }
 }
-programaIniciado();
+paginaIniciada(); /* Inicia la pagina */
